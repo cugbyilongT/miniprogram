ab4db9ed14d09bf5852693eca01c8e860d49a434
unknown <yangyichen615@hotmail.com>
2024-05-14 14:22:57 +0800

    n

--- miniprogram/pages/personalInfo/personalInfo.js
+++ miniprogram/pages/personalInfo/personalInfo.js
@@ -29,34 +29,6 @@ Page({
     onReachBottom: function() {
         // 页面上拉触底事件的处理函数
     },
-    selectTap:function(e){
-        let selectedOptions = {
-            Project: ["大龙街2024年排水管网清、查项目"],
-            Section: [],
-            Task: [],
-            Work: ["清疏台账"],
-            Workername: ["杨怡"],
-          }
-          let dateRange  = {
-            start: "2024-04-08",
-            end: "2024-05-10",
-          }
-            
-          let analysisname = "全部数据"
-        wx.cloud.callFunction({
-            name: 'analysis',
-            data: {
-                selectedOptions: selectedOptions,
-                dateRange: dateRange,
-                analysisname: analysisname
-              },
-    }).then(res => {
-        console.log('云函数查询结果:',res.result)
-    })
-
-
-    },
-
 
     onShareAppMessage: function() {
         // 用户点击右上角分享
